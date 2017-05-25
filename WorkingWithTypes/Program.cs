727cb4438255d2450fff592de13bcb180d173fa6
Geethanjali <test@test.com>
2017-05-25 17:44:57 -0500

    Initial commit

---
+++ WorkingWithTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections;
+namespace WorkingWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B objB = new B();
+            objB.PrintCount();
+        }
+    }
+    public class B
+    {
+        public readonly int count = 5;
+        public B()
+        {
+            count = 10;
+        }
+
+        public void PrintCount()
+        {
+            //count = 15;
+            Console.WriteLine(count);
+        }
+    }
+    public class D:B
+    {
+    }
+}
